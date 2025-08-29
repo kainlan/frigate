f138bbcb7ec2d52d2d3beea4672921a005e792c7
kainlan <cjkolek@gmail.com>
2025-08-29 12:20:55 -0400

    Update install_deps.sh

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -82,6 +82,7 @@ if [[ "${TARGETARCH}" == "amd64" ]]; then
     # intel packages use zst compression so we need to update dpkg
     apt-get install -y dpkg
     apt-get install -y software-properties-common
+    apt-get install --reinstall python3-apt
     # use intel apt intel packages
 
     add-apt-repository -y ppa:kobuk-team/intel-graphics
